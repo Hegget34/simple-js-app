5f9419238bcfed792161836573bc7362f22721de
Hegget34 <Hegget34@hotmail.com>
2022-12-11 22:54:07 -0600

    initial commit

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+let pokidex = [
+
+    {
+        name: 'Squirtle',
+        type: 'water',
+    },
+
+    {
+        name: ('Charmander'),
+        type: ('fire'),
+    },
+
+    {
+        name: 'bulbasaur',
+        type: ('poison', 'grass'),
+    },
+
+    {
+        name: 'Poliwag',
+        type: ('water'),
+    },
+
+    {
+        name: 'Machop',
+        type: ('fighting'),
+    },
+
+]
