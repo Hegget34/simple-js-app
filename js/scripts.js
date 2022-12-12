553c8b7001f2a520909f1434dd055458e6c08f8b
Hegget34 <Hegget34@hotmail.com>
2022-12-11 23:05:03 -0600

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,32 +1,6 @@
-let pokidex = [
 alert('Hello world');
 
-    {
-        name: 'Squirtle',
-        type: 'water',
-    },
-
-    {
-        name: ('Charmander'),
-        type: ('fire'),
-    },
-
-    {
-        name: 'bulbasaur',
-        type: ('poison', 'grass'),
-    },
-
-    {
-        name: 'Poliwag',
-        type: ('water'),
-    },
-
-    {
-        name: 'Machop',
-        type: ('fighting'),
-    },
-
-]let favoriteFood = 'Tacos';
+let favoriteFood = 'Tacos';
 document.write(favoriteFood);
 let foodrating = '10';
 document.write(foodrating);
