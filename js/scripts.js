19393833cbc1dc0733ccf8da6ce03b9ab158a0bc
Hegget34 <Hegget34@hotmail.com>
2022-12-22 18:36:13 -0600

    Updated scripts.js
    Deleted old code and added new variable, along with 3 pokemon to the list. Included - name, height, and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,5 @@
-alert('Hello world');
-
-let favoriteFood = 'Tacos';
-document.write(favoriteFood);
-let foodrating = '10';
-document.write(foodrating);
+let pokemonList = [
+    { name: "Machop", height: 0.8, type: ["Fighting"] },
+    { name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
+    { name: "Poliwag", height: 0.3, type: ["Water"] },
+]
