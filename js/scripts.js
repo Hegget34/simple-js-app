71807bb7cc30db5e8869a2917826e6cc327f6463
Hegget34 <Hegget34@hotmail.com>
2023-01-02 17:57:29 -0600

    added first loop about pokemon height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,22 @@
+/* 
+List of pokemon and details about them
+*/
+
 let pokemonList = [
-    { name: "Machop", height: 0.8, type: ["Fighting"] },
-    { name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
-    { name: "Poliwag", height: 0.3, type: ["Water"] },
+    { ID: "1", name: "Machop", height: 0.8, type: ["Fighting"] },
+    { ID: "2", name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
+    { ID: "3", name: "Poliwag", height: 0.3, type: ["Water"] },
 ]
+
+/* 
+loop to list heights of pokemon
+*/
+
+for (let i = 0; i < pokemonList.length; i++) 
+    {
+    if (pokemonList[i].height > 0.5) {
+        document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Wow! that's taller than most pokemon" + "<br>");
+    } else if (pokemonList[i].height < 0.5) {
+        document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Oh no... That's very small" + "<br>");
+    }
+}
