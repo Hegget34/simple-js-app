c5e6282d2a2918fe63f446442e4863955f8cc7de
Hegget34 <Hegget34@hotmail.com>
2023-01-20 16:03:37 -0600

    Update scripts.js
    changed for loop to forEach

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,39 @@
 List of pokemon and details about them
 */
 
-let pokemonList = [
-    { ID: "1", name: "Machop", height: 0.8, type: ["Fighting"] },
-    { ID: "2", name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
-    { ID: "3", name: "Poliwag", height: 0.3, type: ["Water"] },
+    let pokemonList = [
+        { ID: "1", name: "Machop", height: 0.8, type: ["Fighting"] },
+        { ID: "2", name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
+        { ID: "3", name: "Poliwag", height: 0.3, type: ["Water"] },
 ]
 
 /* 
 loop to list heights of pokemon
 */
 
-for (let i = 0; i < pokemonList.length; i++) 
-    {
+/* printArrayDetails function declaration */
+/* For loop
+
+function printArrayDetails() {
+    for (let i = 0; i < pokemonList.length; i++) 
+        {
+        if (pokemonList[i].height > 0.5) {
+            document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Wow! that's taller than most pokemon" + "<br>");
+        } else if (pokemonList[i].height < 0.5) {
+            document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Oh no... That's very small" + "<br>");
+        }
+    }
+}
+*/
+
+pokemonList.forEach(function (pokemon) {
     if (pokemonList[i].height > 0.5) {
         document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Wow! that's taller than most pokemon" + "<br>");
     } else if (pokemonList[i].height < 0.5) {
-        document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Oh no... That's very small" + "<br>");
+        document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Oh no... That's a very small pokemon" + "<br>");
     }
-}
+});
+
+pokemonRepository.getAll().forEach(function (pokemon) {
+    document.write(pokemonList.name + pokemonList.height);
+});
