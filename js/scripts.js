f083999b2449b391e4d4ad6439b7c99fe4a76aa8
Hegget34 <Hegget34@hotmail.com>
2023-01-20 16:20:24 -0600

    Update scripts.js
    IIFE array input

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
 /* 
 List of pokemon and details about them
 */
-
+let pokemonRepository = (function () {
     let pokemonList = [
         { ID: "1", name: "Machop", height: 0.8, type: ["Fighting"] },
         { ID: "2", name: "Weedle", height: 0.3, type: ["Bug", "Poison"] },
         { ID: "3", name: "Poliwag", height: 0.3, type: ["Water"] },
 ]
+
+    function add(pokemon) {
+        pokemonList.push(pokemon);
+    }
+
+    function getAll() {
+        return pokemonList;
+    }
+
+    return {
+        add: add,
+        getAll: getAll
+    };
+})();
 
 /* 
 loop to list heights of pokemon
@@ -27,6 +41,7 @@ function printArrayDetails() {
 }
 */
 
+/* forEach loop begins */
 pokemonList.forEach(function (pokemon) {
     if (pokemonList[i].height > 0.5) {
         document.write(pokemonList[i].name + " - height = " + pokemonList[i].height + " Wow! that's taller than most pokemon" + "<br>");
