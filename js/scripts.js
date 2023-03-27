d48ffcdb3f9723abbe938c7a2f7386269f60d5c9
Hegget34 <Hegget34@hotmail.com>
2023-03-27 15:34:26 -0500

    added modal
    updated files to input modal and event listeners to close modal in different ways.

--- js/scripts.js
+++ js/scripts.js
@@ -5,23 +5,21 @@ let pokemonRepository = (function () {
 
 /*Add pokemon name and description*/
     function add(pokemon) {
-        if (
-            typeof pokemon === "object" &&
-            "name" in pokemon
-        ) {
-            pokemonList.push(pokemon);
-        } else {
-            console.log("pokemon is not correct");
-        }
-    }
+        pokemonList.push(pokemon);
+    };
+
     function getAll() {
         return pokemonList;
     }
+
+/* button for pokemon to list details when clicked on */
     function addListItem(pokemon) {
         let pokemonList = document.querySelector(".pokemon-list");
         let listpokemon = document.createElement("li");
         let button = document.createElement("button");
         button.innerText = pokemon.name;
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#exampleModal');
         button.classList.add("button-class");
         listpokemon.appendChild(button);
         pokemonList.appendChild(listpokemon);
@@ -41,22 +39,22 @@ let pokemonRepository = (function () {
                     detailsUrl: item.url
                 };
                 add(pokemon);
-                console.log(pokemon);
             });
         }).catch(function (e) {
             console.error(e);
         })
     }
 
+/* details loading in from source */
     function loadDetails(item) {
         let url = item.detailsUrl;
         return fetch(url).then(function (response) {
-        return response.json();
+            return response.json();
         }).then(function (details) {
-            // Details added to the items
+            /* Details added to the items */
             item.imageUrl = details.sprites.front_default;
             item.height = details.height;
-            item.types = details.types;
+            item.types = details.types.map((type) => type.type.name);
         }).catch(function (e) {
             console.error(e);
         });
@@ -64,17 +62,55 @@ let pokemonRepository = (function () {
 
     function showDetails(item) {
         pokemonRepository.loadDetails(item).then(function () {
-            console.log(item);
+            showModal(item);
         });
     }
 
+/* model input starts here */
+    function showModal(pokemon) {
+        let modalBody = $('.modal-body');
+        let modalTitle = $('.modal-title');
+        let modalContainer = $('#modal-container');
+
+        modalBody.empty();
+        modalTitle.text(pokemon.name);
+
+        let height = $('<p id="details" >' + 'Height:  ' + pokemon.height + '</p>');
+        let image = $('<img id="pokemon-img" src="' + pokemon.imageUrl + '" />');
+        let types = $('<p id="details" >' + 'Types:  ' + pokemon.types + '</p>');
+
+        modalBody.append(image);
+        modalBody.append(height);
+        modalBody.append(types);
+
+        modalContainer.show();
+    }
+
+/* hides modal */
+    function hideModal() {
+        let modalContainer = $('#modal-container');
+        modalContainer.hide();
+    } 
+
+    window.addEventListener("keydown", (e) => {
+        if (e.key === "Escape") {
+            hideModal();
+        }
+    });
+
+    window.addEventListener("click", function (event) {
+        hideModal();
+        }
+    );
+    
     return {
         add: add,
         getAll: getAll,
         addListItem: addListItem,
         loadList: loadList,
         loadDetails: loadDetails,
-        showDetails: showDetails
+        showDetails: showDetails,
+        showModal: showModal
     };
 })();
 
@@ -84,5 +120,3 @@ pokemonRepository.loadList().then(function () {
         pokemonRepository.addListItem(pokemon);
     });
 });
-
-
